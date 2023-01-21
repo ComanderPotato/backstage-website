aa909b13a00b74495080bebcc802f6ad0a22b433
Tom Golding <tomgolding2012@outlook.com>
2023-01-21 14:23:42 +1100

    Commited website code and assets

---
+++ script.ts
@@ -0,0 +1,29 @@
+const sectionEl = document.querySelectorAll(
+  "section"
+) as NodeListOf<HTMLElement>;
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let issueNum = entry.target.getAttribute("data-issue");
+        let scroll =
+          entry.target.getBoundingClientRect().top -
+          document.body.getBoundingClientRect().top;
+
+        window.scrollTo({
+          top: scroll,
+          behavior: "smooth",
+        });
+        document.body.style.backgroundColor = `var(--issue-${issueNum}`;
+      }
+    });
+  },
+  { threshold: 0.05 }
+);
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    document.body.style.backgroundColor = "white";
+  }
+});
+sectionEl.forEach((el) => observer.observe(el));
